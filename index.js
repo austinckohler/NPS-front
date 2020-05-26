5dd999adab8f444097d8eb34769fba59f0993fa9
austinckohler <austinckohler@gmail.com>
2020-05-26 13:19:59 -0600

    added login and log out for existing users

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const userSignupForm = document.querySelector("#new-user")
+const userLoginForm = document.querySelector("#login")
+const isLoggedIn = document.querySelector(".is-logged-in")
 
+function isLoggedIn() {
+    isLoggedIn.textContent = localStorage.getItem("token")
+    ? "You are logged in."
+    : "No user is logged in."
+}
+
+function isLoggedOut(){
+    localStorage.removeItem("token")
+    isLoggedIn()
+}
 userSignupForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
@@ -19,3 +31,27 @@ userSignupForm.addEventListener("submit", (event) => {
         window.location.reload()
     })
 })
+
+
+userLoginForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+
+    const username = formData.get("username")
+    const password = formData.get("password")
+
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify({ username, password }), 
+    }).then(response => response.json())
+        .then(response => {
+            const { token } = response 
+            localStorage.setItem("token", token)
+            isLoggedIn()
+        })
+    })
